c741f9d53ba8af364322d7f50e5455633837e52c
&blueblood <khashchuk.denis@gmail.com>
2024-04-22 21:21:23 +0300

    pizda

--- Lab2/Isu.Extra/Exceptions/EnrollException.cs
+++ Lab2/Isu.Extra/Exceptions/EnrollException.cs
@@ -7,12 +7,12 @@ public class EnrollException : Exception
         : base(message)
     {
     }
-// dsdsdsds
+// dsdsdsdsdsdsdsds
     public static EnrollException StudentToEnrollNotFoundException()
     {
         return new EnrollException("student wasn't found");
     }
-
+// dsdsdsdsdsdsds
     public static EnrollException WrongFacultyDuringEnrollException()
     {
         return new EnrollException("can't enroll student to this course as faculties are similar");
